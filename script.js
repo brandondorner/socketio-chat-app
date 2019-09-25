1f9c98ecd64aa0d7b66ade6e2056fa8a13911efc
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-09-25 14:52:22 -0500

    message sending and user connecting messages

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ const messageForm = document.getElementById('text-form')
 const messageInput = document.getElementById('message-input')
 const messageContainer = document.getElementById('message-container')
 
+
+//takes incoming message and creates a new div with message text
+const appendMessage = (message) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
+
+//Get username and join lobby, send username to other users
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
 //whenever a message is sent, run this function
 socket.on('chat-message', data => {
     appendMessage(data)
+})
+
+//whenever a new user joins, run this
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
 })
 
 //listening to form
@@ -20,10 +38,3 @@ messageForm.addEventListener('submit', e => {
     //clears message form
     messageInput.value=''
 })
-
-//takes incoming message and creates a new div with message text
-const appendMessage = (message) => {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
-}
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const io = require('socket.io')(3000)
 
+const users = {}
+
 //Whenever user loads site, do this 
 io.on('connection', socket => {
+    //when server recieves new user, do this
+    socket.on('new-user', name => {
+        //broadcast a different user has connected to all other users
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+
     //server receiving message. send-chat-message is the name of the message, message is the actual input value
     socket.on('send-chat-message', message => {
         //sends message to every other user
