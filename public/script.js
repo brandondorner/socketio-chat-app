84566959c1a999675b66aed5ecb9ff974fc568b9
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-10-01 22:42:57 -0500

    fixed the problem, app fully functional

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ const socket = io('http://localhost:3000')
 const messageForm = document.getElementById('text-form')
 const messageInput = document.getElementById('message-input')
 const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
 
 //takes incoming message and creates a new div with message text
 const appendMessage = (message) => {
@@ -16,7 +17,7 @@ if (messageForm != null){
     //Get username and join lobby, send username to other users
     const name = prompt('What is your name?')
     appendMessage('You joined')
-    socket.emit('new-user', name)
+    socket.emit('new-user', roomName, name)
 
     //listening to form
     messageForm.addEventListener('submit', e => {
@@ -24,17 +25,28 @@ if (messageForm != null){
         //grabbing input value 
         const message = messageInput.value
         //send info from client to server
-        socket.emit('send-chat-message', message)
-        //clears message form
-        messageInput.value=''
+        socket.emit('send-chat-message', roomName, message)
         //adds the users message to his own chat
         appendMessage(`You: ${message}`)
+        //clears message form
+        messageInput.value=''
+
 
 })
 }
 
-socket.on('room-created', newRoom => {
-    
+//whenever new room is created, do this
+socket.on('room-created', room => {
+    //create new room element
+    const roomElement = document.createElement('div')
+    roomElement.innerText = room
+    //create the new room a tag with correct href
+    const roomLink = document.createElement('a')
+    roomLink.href = `/${room}`
+    roomLink.innerText = 'join'
+    //append the link and element to the homepage container
+    roomContainer.append(roomElement)
+    roomContainer.append(roomLink)
 })
 
 //whenever a message is sent, run this function
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 
 //zero rooms to start
-const rooms = { room1: {}} 
+const rooms = { } 
 
 //routes
 //at route index, render inex file as well as rooms
@@ -23,49 +23,74 @@ app.get('/', (req, res) => {
 })
 
 app.post('/room', (req, res) => {
+    console.log(req)
     //if new room already exists, redirect to index
-    if (rooms[req.body.newRoom] != null){
+    if (rooms[req.body.room] != null){
         return res.redirect('/')
     }
-    //adds new-room to rooms object
-    //add an empty list of users to the newly created room key. then redirect to page
-    rooms[req.body.newRoom] = { users: {} }
-    res.redirect(req.body.newRoom)
+    //adds newRoom, from script.js, to rooms object
+    //add an empty list of users to the newly created room key. 
+    //then redirect to page
+    rooms[req.body.room] = {users: {}} //
+    res.redirect(req.body.room)
     //send message that new room was created to other users
-    io.emit('room-created', req.body.newRoom)
+    io.emit('room-created', req.body.room)
 })
 
 //when given the url parameter of room, render the room file
 //and store the room parameter in the roomName variable
+//and pass roomName variable to room.ejs
 app.get('/:room', (req, res) => {
     //if room doesn't exist, redirect to homepage
-    if (rooms[req.params.room] == null) {
+    if (rooms[req.params.room] == null) {//newroom
         return res.redirect('/')
     }
-    res.render('room', { roomName: req.params.room})
+    res.render('room', { roomName: req.params.room})//
 })
 
 server.listen(3000)
 
-const users = {}
-
 //Whenever user loads site, do this 
 io.on('connection', socket => {
-    //when server recieves new user, do this
-    socket.on('new-user', name => {
-        //broadcast a different user has connected to all other users
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
+    //when server receives new user, do this
+    socket.on('new-user', (room, name) => {
+        //join a room then
+        console.log(room, 'r2')
+        socket.join(room)
+        //access roomName out of rooms var, then grab the users from that room and assign the specific user the inputed name
+        console.log(rooms)
+        console.log(name)
+        rooms[room].users[socket.id] = name
+        //broadcast user (name) has connected to all other users in param 'roomName'
+        socket.to(room).broadcast.emit('user-connected', name)
     })
 
     //server receiving message. send-chat-message is the name of the message, message is the actual input value
-    socket.on('send-chat-message', message => {
-        //sends {username and message} to every other user
-        socket.broadcast.emit('chat-message', { name: users[socket.id], message: message  })
+    socket.on('send-chat-message', (roomName, message) => {
+        //sends {(user)name and message} to script.js 
+        socket.to(roomName).broadcast.emit('chat-message', { message: message,  name: rooms[roomName].users[socket.id]  })
     })
-    //when user leaves chat, broadcast to ther users and delete the user who left
+    //when user leaves chat, broadcast to the users and delete the user who left
     socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
+        getUserRooms(socket).forEach(roomName => {
+            //when user disconnects, broadcast this event and delete user from the room
+            socket.to(roomName).broadcast.emit('user-disconnected', rooms[roomName].users[socket.id])
+            delete rooms[roomName].users[socket.id]
+        })
+
     })
 })
+
+//check all rooms, and return all rooms that the user is a part of
+const getUserRooms = (socket) => {
+    //object entries is a method that converts that rooms object into an array of entries
+    //roomName, room is the key value pair for rooms. names is the accumulator
+    return Object.entries(rooms).reduce((names, [roomName, room]) => {
+        //if user exists, push name of the room to names (list of all room names)
+        console.log('getuserrooms', rooms)
+        if (room.users[socket.id] != null){
+            names.push(roomName)
+        }
+        return names
+    }, [])
+}
